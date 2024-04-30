dd65694d6c06377472680401f8beb8634e209e5b
hfccr <hfccr@outlook.com>
2024-04-30 17:10:41 +0530

    Add token id retrieval

--- packages/hardhat/contracts/SampleNFT.sol
+++ packages/hardhat/contracts/SampleNFT.sol
@@ -19,4 +19,17 @@ contract SampleNFT is ERC721 {
 		uint256 newTokenId = _tokenIds.current();
 		_mint(msg.sender, newTokenId);
 	}
+
+	function getAllTokensOwnedByAddress(address _owner) external view returns (uint256[] memory) {
+		uint256 tokenCount = balanceOf(_owner);
+		uint256[] memory tokenIds = new uint256[](tokenCount);
+		uint256 tokensFound = 0;
+		for (uint256 i = 1; i <= _tokenIds.current(); i++) {
+			if (_owner == ownerOf(i)) {
+				tokenIds[tokensFound] = i;
+				tokensFound++;
+			}
+		}
+		return tokenIds;
+	}
 }
